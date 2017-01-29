1e3e8387bed13626bcf6742e96420a6f6feaf2d5
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-01-29 17:51:12 -0600

    finishe side

--- libs/entradas.php
+++ libs/entradas.php
@@ -39,7 +39,49 @@ if ( $the_query->have_posts() ) {
 /* Restore original Post Data */
 wp_reset_postdata();  
 }
-/*=====================================*/
+/*========== Last publicaciones ========================*/
+
+function last_publicaciones(){
+/*==================================*/
+    /**
+     * The WordPress Query class.
+     * @link http://codex.wordpress.org/Function_Reference/WP_Query
+     *
+     */
+    $args_10 = array(
+        //Post & Page Parameters
+        'post__not_in' => array(2883),
+        'post_parent__not_in'  => array(2883),
+        'post_type' => 'any',
+        'post_status' => 'publish',
+        //Order & Orderby Parameters
+        'order'               => 'DESC',
+        'orderby'             => 'date',
+        //Pagination Parameters
+        'posts_per_page'         => 7,
+        'posts_per_archive_page' => 10,
+    );
+
+  // The Query
+$the_query = new WP_Query( $args_10);    
+// The Loop
+if ( $the_query->have_posts() ) {
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        ?>
+<div class="list">
+    <a class="thumb" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a> 
+    <a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+</div>
+        <?php }} else {
+        echo 'No hay artículos';    
+    // no se encontraron artículos
+}
+/* Restore original Post Data */
+ wp_reset_postdata();  
+}
+
+/*=========== Ultimas páginas ==========================*/
 function ultimas_paginas(){
 /*==================================*/
     /**
@@ -49,8 +91,8 @@ function ultimas_paginas(){
      */
     $args_10 = array(
         //Post & Page Parameters
-        'post__not_in' => array(214),
-        'post_parent__not_in'  => array(214),
+        'post__not_in' => array(2883),
+        'post_parent__not_in'  => array(2883),
         'post_type' => 'any',
         'post_status' => 'publish',
         //Order & Orderby Parameters
